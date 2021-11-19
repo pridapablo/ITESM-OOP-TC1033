24333cca280cd6a8a7d5924a127452ee57a2e7c3
pridapablo <pridapablo@gmail.com>
2021-11-18 22:11:06 -0600

    commit

--- Tarea2/T2_Ej2_Supermercado.cpp
+++ Tarea2/T2_Ej2_Supermercado.cpp
@@ -10,7 +10,7 @@ C. Cada departamento es una función que recibe un cliente, lo saluda “hola Pe
 bienvenido” y pregunta al cliente cuánto gastó. La cantidad que responda el 
 cliente deberá sumarse al atributo total gastado de cada cliente.  
 
-/NO HACER D. Crea una función que imprima el total gastado por los clientes que visitaron hoy el 
+D. Crea una función que imprima el total gastado por los clientes que visitaron hoy el 
 supermercado (la función recibe como parámetro el arreglo de clientes y suma el 
 total gastado por cada uno. Al final la función imprime “hola, soy Shedrawiwi y 
 hoy gane xxxxx pesos con n clientes” 
@@ -35,64 +35,69 @@ struct Cliente //Inciso A
 };
 
 // Incisos B y C: funciones departamentos
-void frutas_y_verduras(std::string *nombre, int *acumulador)
+void frutas_y_verduras(std::string *nombre, int *gasto_cliente, int *ganancia)
 {
     int total;
     std::cout << "Hola " << *nombre << ", bienvenido \n";
     std::cout << "¿Cuánto gastaste en frutas y verduras? ";
     std::cin >> total;
     std::cout << "Gracias por gastar " << total << " pesos en frutas y verduras \n";
-    (*acumulador) += total;
+    (*gasto_cliente) += total;
+    (*ganancia) += total;
 }
-void carnes_y_congelados(std::string *nombre, int *acumulador)
+void carnes_y_congelados(std::string *nombre, int *gasto_cliente, int *ganancia)
 {
     int total;
     std::cout << "Hola " << *nombre << ", bienvenido \n";
     std::cout << "¿Cuánto gastaste en carnes y congelados? ";
     std::cin >> total;
     std::cout << "Gracias por gastar " << total << " pesos en carnes y congelados \n";
-    (*acumulador) += total;
+    (*gasto_cliente) += total;
+    (*ganancia) += total;
 }
-void vinos_y_licores(std::string *nombre, int *acumulador)
+void vinos_y_licores(std::string *nombre, int *gasto_cliente, int *ganancia)
 {
     int total;
     std::cout << "Hola " << *nombre << ", bienvenido \n";
     std::cout << "¿Cuánto gastaste en vinos y licores? ";
     std::cin >> total;
     std::cout << "Gracias por gastar " << total << " pesos en vinos y licores \n";
-    (*acumulador) += total;
+    (*gasto_cliente) += total;
+    (*ganancia) += total;
 }
-void dulceria(std::string *nombre, int *acumulador)
+void dulceria(std::string *nombre, int *gasto_cliente, int *ganancia)
 {
     int total;
     std::cout << "Hola " << *nombre << ", bienvenido \n";
     std::cout << "¿Cuánto gastaste en dulcería? ";
     std::cin >> total;
     std::cout << "Gracias por gastar " << total << " pesos en dulcería \n";
-    (*acumulador) += total;
+    (*gasto_cliente) += total;
+    (*ganancia) += total;
 }
-void articulos_de_limpieza(std::string *nombre, int *acumulador)
+void articulos_de_limpieza(std::string *nombre, int *gasto_cliente, int *ganancia)
 {
     int total;
     std::cout << "Hola " << *nombre << ", bienvenido \n";
     std::cout << "¿Cuánto gastaste en artículos de limpieza? ";
     std::cin >> total;
     std::cout << "Gracias por gastar " << total << " pesos en artículos de limpieza \n";
-    (*acumulador) += total;
+    (*gasto_cliente) += total;
+    (*ganancia) += total;
 }
 
-// Inciso E: función gasto 
-void gasto(Cliente *nombre, Cliente *acumulador)
+// Inciso E: función ganancia 
+void ganancia_del_dia(/*Contacto** lista,*/ int *ganancia);
 {
-    std::cout << "Hola, soy " << *nombre << " y gasté " << *acumulador << " en el supermercado \n";
+    std::cout << "El día de hoy, el super ganó $" << *ganancia << "\n";
 }
 
 int main ()
 {
-    Cliente* lista_de_clientes; 
+    Cliente** lista_de_clientes; 
     int size_clientes = 10;
     int numero_elementos_agregados = 0;
-    lista_de_clientes = new Cliente[size_clientes];// Inciso E: arreglo con clientes
+    lista_de_clientes = new Cliente*[size_clientes];// Inciso E: arreglo con clientes
 
     Cliente juanito; //instancia
     juanito.nombre = "Juanito";
@@ -119,8 +124,14 @@ int main ()
     lorenita.teléfono = "+52 55 2967 3542";
     lorenita.total_gastado = 0;
 
-    frutas_y_verduras(&juanito.nombre, &juanito.total_gastado);
-    gasto(lista_de_clientes->nombre, &juanito.total_gastado);
+    int ganancia_super = 0;
+
+    frutas_y_verduras(&juanito.nombre, &juanito.total_gastado, &ganancia_super);
+    frutas_y_verduras(&lorenita.nombre, &lorenita.total_gastado, &ganancia_super);
+
+    ganancia_del_dia(/*Contacto** lista,*/ &ganancia_super);
+
+    //gasto(lista_de_clientes,size_clientes);
 
     return 0;
 }
